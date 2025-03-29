a7ae8e4b576fc82973817454b39c0b6d98de28cb
ThiagoRiey <153005132+ThiagoRiey@users.noreply.github.com>
2025-03-29 13:45:43 -0300

    Update Metodo_construtor.py

--- Metodo_construtor.py
+++ Metodo_construtor.py
@@ -1,5 +1,5 @@
 class Pessoa:
-  def __init__(self, nome, idade, cpf):
+  def __init__(self, nome, idade, cpf): #__init__  é o método construtor em python
     self.nome = nome
     self.idade = idade
     self.cpf = cpf
