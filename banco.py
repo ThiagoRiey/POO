8b3e201b7f05ad90e3024e03800201d9ed24f738
ThiagoRiey <153005132+ThiagoRiey@users.noreply.github.com>
2025-03-29 14:11:34 -0300

    Update banco.py

--- banco.py
+++ banco.py
@@ -1,87 +1,93 @@
-class Banco:
-  def __init__(self, nome, saldo=0):
-    self.nome = nome
-    self.saldo = saldo
-    self.senha = none
+class banco:
+  # Método construtor
+  def __init__(self, banco, agencia, conta, cliente, cpf):
+    self.banco = banco
+    self.agencia = agencia
+    self.conta = conta
+    self.cliente = cliente
+    self.cpf = cpf
+    self.saldo = 0
+    self.senha = None
 
-  def cadastrar_senha(self):
-    if not self.senha:
-      self.senha = input("Cadastrar sua senha: ")
-      print("Senha cadastrada com sucesso!")
+  # Método de definição de senha
+  def definir_senha(self, new_senha):
+    if self.senha == None:
+        self.senha = new_senha
+        print('Sua senha foi definida!')
+
+    elif self.senha != None:
+        decisao = input("Você já possui uma senha definida! Gostaria de mudar mesmo assim ? : ").lower
+          if decisao == "sim":
+              self.senha = new_senha
     else:
-      print("Senha já cadastrada.")
+        print("Escolha errada!")
+  # Método de apresentação
+  def __srt__(self):
+    return f"""
+Cliente {self.cliente} cadastrado com sucesso
+Conta: {self.conta}
+Agencia: {self.agencia}
+Banco: {self.banco}
+""" 
+  # deposito
+  def Deposito(self, valor):
+    if isinstance(valor, (int, float)):
+        self.saldo += valor
+        print(f"Saldo atualizado com sucesso no valor de R${float(valor)}")
 
-  def verificar_senha(self):
-    tentativa = input("Digite sua senha: ")
-    return tentativa == self.senha
-
-  def depositar(self, valor):
-    if valor > 0:
-      self.saldo += valor
-      print(f'Depósito de R${valor} realizado com sucesso"')
+  # saque
+  def Saque(self, destinatario, valor):
+    if self.senha == senha and salf.saldo >= valor and isinstance(valor, (int, float)):
+      self.saldo -= valor
+      print(f"Saque realizado no valor de: R${float(valor)}")
     else:
-      print("Valor de depósito inválido.")
+      print("Valor ou senha incorreta, tente novamente")
 
-  def consultar_saldo(self):
-    print(f'Saldo atual: R${self.saldo}')
+  # Pix
+  def Pix(self, destinatario, valor):
+    if isinstance(valor, (int, float)) and self.saldo >= valor:
+      self.saldo -= valor
+      destinatario.saldo += valor
+                      f"""
+Pix realizado no valor de R${valor}
+De: {self.cliente}
+Para: {destinatario.cliente}
+"""
+            )
+    # Extrato
+    def Extrato(self):
+        print(
+            f"""
+-----------------------------------
+Conta? {self.conta}
+Agencia: {self.agencia}
+Saldo: {self.saldo}
+Cliente: {self.cliente}
+-----------------------------------
+            """
+        )
+zecove = Banco('Inter', 1, 1006, 'Ze das Coves', "987654321")
 
-  def apresentar(self):
-    print(f"Cliente: {self.nome}, Saldo: R${self.saldo}")
+ze = Banco(" Saqua Eng Software", 1 , 1, "Ze da Manga", "123456789")
 
-  def transferir(self, destinatario, valor):
-    if self.saldo >= valor and valor > 0:
-       if self.verificar_senha():
-        self.saldo -= valor
-        destinatario.saldo += valor
-        print(f'Tranferência de R${valor} realizada para {destinatario.nome}.')
-       else:
-        print('Senha incorreta. transferência cancelada.')
-    else:
-      print("Transeferência inválida. Saldo insuficiente ou valor incorreto.")
+ze.definir_senha('admin01')
 
-@classmethod
-def criar_conta(cls):
-  nome = input("Digite o seu nome: ")
-  cliente = cls(nome)
-  cliente.cadastrar_senha()
-  return cliente
+ze.Deposito(5000)
 
-@classmethod
-def selecionar_conta(cls, clientes):
-  if len(clientes) == 0:
-     print("Nenhuma conta disponível. Crie uma conta primeiro.")
-     return None
+ze.Saque("admin01", 2500)
 
-  print('=== Selecionar Conta ===')
-  for idx, cliente in enumerate(clientes):
-     print(f"{idx + 1}. {cliente.nome}")
+ze.Extrato()
 
-  opcao = int(input("Escolha o número da conta: "))
-  if 1 <= opcao <= len(clientes):
-    return clientes[opcao - 1]
-  else:
-      print("Opção inválida.")
-      return None
+zecove.definir_senha(5556666)
 
-      while True:
-         print('=== menu do Banco ===')
-         print('1. Criar Conta')
-         print('2. Selecionar Conta')
-         print('3. Depositar')
-         print('4. Consultar saldo')
-         print('5. Apresentar dados')
-         print('6. transferir')
-         print('7. Sair')
+ze.Pix(zecove, 2000)
 
-         opcao = input("Escolha uma opção: ")
-         if opcao == '1':
-            novo_cliente = cls.criar_conta()
-            cliente.append(novo_cliente)
-         elif opcao == "2":
-            cliente_selecionado = cls.selecionar_conta(clientes)
-         elif opcao == '3':
-            if cliente_selecionado:
-              valor = float(input("Informe o valor para depósito: "))
-              cliente_selecionado.depositar(valor)
-            else:
-              print("Você precisa selecionar uma conta prinmeiro!")
+ze.Saque("admin01", 1000)
+
+zecove.Extrato()
+
+print(ze)
+
+print(zecove)
+        
+  
